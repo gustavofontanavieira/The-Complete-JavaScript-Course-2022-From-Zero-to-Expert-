b7ffd295d60a47a1c2f1733cc1b524cfbf79734a
Gustavo Fontana Vieira <83253065+gustavofontanavieira@users.noreply.github.com>
2022-08-02 21:36:06 -0300

    Pig Game - starting

---
+++ Pig Game/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Selecionando elementos
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
